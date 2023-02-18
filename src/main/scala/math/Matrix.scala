f155e5674e1a81448674c3f8d7dea1fa9a1a1704
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2023-02-18 17:43:09 +0000

    Decryption working

--- src/main/scala/math/Matrix.scala
+++ src/main/scala/math/Matrix.scala
@@ -37,8 +37,30 @@ case class Matrix(rowCount: Int, colCount: Int, rows: List[List[Int]]) {
     rows.map(row => row.zip(vector).map(x => x._1 * x._2).sum)
 
   // Generate a matrix with an inverse
-  def generate(): (Matrix, Matrix) = ???
+  def generate2x2(mod: Int): (Matrix, Matrix) = ???
 
-  // Generate a matrix with an inversse mod an int
-  def generate(mod: Int): (Matrix, Matrix) = ???
+  // Invert the 2x2 matrix
+  def invert2x2(mod: Option[Int]): Option[Matrix] = {
+    val determinant = rows(0)(0) * rows(1)(1) - rows(0)(1) * rows(1)(0)
+
+    val determinantsInverse: Int = mod match {
+      case None =>
+        throw new Exception(
+          "Only modular matrices are supported for now, so only integers can be stored in them"
+        )
+      case Some(mod) =>
+        Congruence
+          .solve(determinant, 1, mod)
+          .getOrElse(return None)
+    }
+
+    Some(
+      (this.copy(
+        rows = List(
+          List(rows(1)(1), -rows(0)(1)),
+          List(-rows(1)(0), rows(0)(0))
+        )
+      ) * determinantsInverse) % mod.get
+    )
+  }
 }
