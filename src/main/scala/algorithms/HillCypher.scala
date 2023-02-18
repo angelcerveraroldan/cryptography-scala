c913f534c521a98b06d1575b6d15cf2cebdb69ec
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2023-02-18 17:45:50 +0000

    Decryption working (#1)

--- src/main/scala/algorithms/HillCypher.scala
+++ src/main/scala/algorithms/HillCypher.scala
@@ -11,10 +11,12 @@ case class HillCypher(providedKey: Option[Matrix] = None)
     extends EncryptionAlgo {
   val defaultKey: Matrix = Matrix(2, 2, List(List(3, 3), List(2, 5)))
   val key = providedKey.getOrElse(defaultKey)
+  val inverseOfKey = key
+    .invert2x2(Some(95))
+    .getOrElse(throw new Exception("Please use a key with an inverse mod 95"))
 
   def encryptString(s: String): String = {
-    s
-      .grouped(2)
+    s.grouped(2)
       .flatMap(chars => encryptVec(chars.toList))
       .foldRight("")(_ + _)
   }
@@ -27,41 +29,39 @@ case class HillCypher(providedKey: Option[Matrix] = None)
         .foreach(chars => {
           fileWriter.write(encryptVec(chars.toList).toArray)
         })
-
-      fileWriter.close()
     }
+
+    fileWriter.close()
   }
 
   def decrypt(path: String, to: String): Unit =
-    HillCypher(
-      Some(
-        Matrix(
-          2,
-          2,
-          List(
-            List(85, 63),
-            List(42, 32)
-          )
-        )
-      )
-    ).encryptFile(path, to)
+    HillCypher(providedKey = Some(inverseOfKey)).encryptFile(path, to)
 
   /*
    * If the pair has a special character such as newline or break-line
    * then we will not encrypt it
    * */
   private def encryptVec(vec: List[Char]): List[Char] = vec match {
+    // If the pair of chars has a character that we cannot encrypt, we will just leave those two characters un-encrypted
     case list if list.exists(_.toInt < 32) => list
+    // If the list only contains one element, we will add a space at the beggining
     case List(x) =>
       (key * List(' ', x).map(_.toInt - 32))
         .map(encryptedAsciiVal =>
           (Math.floorMod(encryptedAsciiVal, (95)) + 32).toChar
         )
         .tail
-    case _ =>
-      (key * vec.map(_.toInt - 32))
-        .map(encryptedAsciiVal =>
-          (Math.floorMod(encryptedAsciiVal, (95)) + 32).toChar
-        )
+    case l =>
+      /*
+       * This will happen at max one time, so the tail works well. If we change the algorithm to group string into n
+       * chars, then this should change, but for that we also need to add a lot of features to the Matrix
+       * */
+      
+      if (l.length == 1) encryptVec(' ' +: vec).tail
+      else
+        (key * vec.map(_.toInt - 32))
+          .map(encryptedAsciiVal =>
+            (Math.floorMod(encryptedAsciiVal, (95)) + 32).toChar
+          )
   }
 }
--- src/test/scala/math/MatrixTest.scala
+++ src/test/scala/math/MatrixTest.scala
@@ -9,6 +9,8 @@ class MatrixTest extends AnyFlatSpec with Matchers {
   val matrix2 = Matrix(2, 2, List(List(1, 1), List(2, 4)))
   val matrix3 = Matrix(2, 2, List(List(2, 5), List(12, 15)))
 
+  val matrix4: Matrix = Matrix(2, 2, List(List(3, 3), List(2, 5)))
+
   behavior of "Matrix x Scalar => Matrix"
 
   it should "Matrix times scalar" in {
@@ -65,5 +67,38 @@ class MatrixTest extends AnyFlatSpec with Matchers {
 
   it should "vector product" in {
     matrix3 * List(32, 12) shouldBe List(124, 564)
+  }
+
+  behavior of "Matrix inverse"
+
+  it should "inverse modulus 95" in {
+    matrix4.invert2x2(Some(95)).get shouldBe Matrix(
+      2,
+      2,
+      List(
+        List(85, 63),
+        List(42, 32)
+      )
+    )
+  }
+
+  it should "inverse mod 17" in {
+    matrix2.invert2x2(Some(17)).get shouldBe Matrix(
+      2,
+      2,
+      List(List(2, 8), List(16, 9))
+    )
+  }
+
+  it should "inverse mod 93" in {
+    matrix2.invert2x2(Some(17)).get shouldBe Matrix(
+      2,
+      2,
+      List(List(2, 8), List(16, 9))
+    )
+  }
+
+  it should "give None when there is no inverse" in {
+    matrix2.invert2x2(Some(30)) shouldBe None
   }
 }
