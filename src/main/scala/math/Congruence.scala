46c7cb61ee4ef507625ef8fd923463fa3bb77f6e
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2023-02-19 11:47:43 +0000

    Better comments

--- src/main/scala/math/Congruence.scala
+++ src/main/scala/math/Congruence.scala
@@ -4,13 +4,16 @@ object Congruence {
   /*
    * Find the smallest k such that
    * x⋅k ≡ y  (mod n)
-   *
-   * TODO: Optimize
    * */
   def solve(x: Int, y: Int, n: Int): Option[Int] =
-    (0 to n).find { num =>
-      x * num % n == y
-    }
+    (0 to n).find(potentialAnswer => x * potentialAnswer % n == y)
 
+  /*
+   * Find the inverse of a number mod, this doesnt always exist, so we use an option#
+   *
+   * Example:
+   * The inverse of 9 mod 26 is 3, this is because
+   * 9⋅3 = 27 ≡ 1  (mod 26)
+   * */
   def inverse(num: Int, mod: Int): Option[Int] = solve(num, 1, mod)
 }
